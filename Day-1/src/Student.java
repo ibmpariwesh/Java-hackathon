297d4a813bdfc9cf8a0a37df81a282a4a7e3754c
ibmjavanh01 <ibmjavanh01@iiht.tech>
2021-03-09 17:27:33 +0530

    initial commit

---
+++ Day-1/src/Student.java
@@ -0,0 +1,12 @@
+
+public class Student extends Human{
+	void study() {
+		System.out.println(getName()+" is studying");
+	}
+	Student(){//default constructor
+		
+	}
+	Student(String name){ //custom constructor
+		setName(name);
+	}
+}
